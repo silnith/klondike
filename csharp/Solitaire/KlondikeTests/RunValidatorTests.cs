313be379433c9f99ffc051bacd4dad4c774fc486
silnith <silnith@gmail.com>
2025-01-12 17:54:31 -0600

    Prune unused using statements.

--- csharp/Solitaire/KlondikeTests/RunValidatorTests.cs
+++ csharp/Solitaire/KlondikeTests/RunValidatorTests.cs
@@ -2,9 +2,6 @@
 using Silnith.Game.Deck;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Silnith.Game.Klondike.Tests
 {
